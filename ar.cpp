520f9a03d782efa4d1d1fabced15a4885aaac341
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-07-21 00:38:01 +0530

    it is a one of the c++

---
+++ ar.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      int n;
+    int a[20]={12,45,56,23};
+    cout<<"enter any no which up to print"<<"\n";
+    cin>>n;
+    for(int i=0;i<5;i++)
+    {
+        cout<<"\n"<<a[i];
+    }
+    return 0;
+}
