581e8341f37a3a1daa1462c9c18855109bcd6ed6
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-09-06 00:18:08 +0530

    it is a some practice

---
+++ ra15.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+    while(i<=10)
+    {
+        if(i==1)
+        {
+            i++;
+            continue;
+        }
+        cout<<i<<endl;
+        i++;
+    }
+}
